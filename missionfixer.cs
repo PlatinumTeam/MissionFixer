2efea14d4f654408e2307758bd4dc9f4907ee07a
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-05-28 13:51:57 -0400

    formatting

--- missionfixer.cs
+++ missionfixer.cs
@@ -20,76 +20,76 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
- 
+
 function fixMissions()
 {
-   echo("Fixing missions in progress.");
-   echo("It might take a while depending on how many mission files there are.");
-   echo("This will ignore leaderboard missions.");
-   echo("REMEMBER: when you are finished fixing, remove missionFix.cs and missionFix.cs.dso");
-   echo("          so that the anti hack will stop in the leaderboards.");
-   
-   // in 50 milliseconds, we will start.  Give the system time to show the echo.
-   schedule(50, 0, fixThoseMissions);
+	echo("Fixing missions in progress.");
+	echo("It might take a while depending on how many mission files there are.");
+	echo("This will ignore leaderboard missions.");
+	echo("REMEMBER: when you are finished fixing, remove missionFix.cs and missionFix.cs.dso");
+	echo("          so that the anti hack will stop in the leaderboards.");
+
+	// in 50 milliseconds, we will start.  Give the system time to show the echo.
+	schedule(50, 0, fixThoseMissions);
 }
- 
+
 function fixThoseMissions()
 {
-   // input and output streams
-   %fIn  = new FileObject();
-   %fOut = new FileObject();    
-   
-   // Itterate through all of the mission files.
-   %search = "*.mis";
-   %file = findFirstFile(%search);
-   while (%file !$= "")
-   {
-      // ignore Lb files
-      if (strStr(%file, "lbmissions") != -1 || strStr(%file, "lb_custom") != -1)
-         continue;      
-     
-      // grab the contents of every line in the file so that we can rewrite it
-      if (%fIn.openForRead(%file))
-      {
-         %count = 0;
-         while (!%fIn.isEOF())
-         {
-            // Jeff: parse the line
-            %currLine = %fIn.readLine();
-           
-            // Jeff: replace occurrences of marble/data | platinum/data | ~
-            // with $usermods @ "/data
-            %firstWord = firstWord(trim(%currLine));
-            if (%firstWord $= "interiorFile" || %firstWord $= "interiorResource")
-            {
-               %currLine = strreplace(%currLine, "\"marble/data", "$usermods @ \"/data");
-               %currLine = strreplace(%currLine, "\"platinum/data", "$usermods @ \"/data");
-               %currLine = strreplace(%currLine, "\"~/data", "$usermods @ \"/data");
-                           
-                           // this will auto correct any errors
-                           %currLine = strreplace(%currLine, "\"$usermods @ \"/data", "$usermods @ \"/data");
-            }
-           
-            %line[%count] = %currLine;
-            %count ++;
-         }
-      }
-      %fIn.close();
-     
-      // now we write it out!
-      if (%fOut.openForWrite(%file))
-      {
-         for (%i = 0; %i < %count; %i ++)
-            %fOut.writeLine(%line[%i]);
-      }
-      %fOut.close();
-     
-      %file = findNextFile(%search);
-   }
-   
+	// input and output streams
+	%fIn  = new FileObject();
+	%fOut = new FileObject();    
+
+	// Itterate through all of the mission files.
+	%search = "*.mis";
+	%file = findFirstFile(%search);
+	while (%file !$= "")
+	{
+		// ignore Lb files
+		if (strStr(%file, "lbmissions") != -1 || strStr(%file, "lb_custom") != -1)
+			continue;      
+
+		// grab the contents of every line in the file so that we can rewrite it
+		if (%fIn.openForRead(%file))
+		{
+			%count = 0;
+			while (!%fIn.isEOF())
+			{
+				// Jeff: parse the line
+				%currLine = %fIn.readLine();
+
+				// Jeff: replace occurrences of marble/data | platinum/data | ~
+				// with $usermods @ "/data
+				%firstWord = firstWord(trim(%currLine));
+				if (%firstWord $= "interiorFile" || %firstWord $= "interiorResource")
+				{
+					%currLine = strreplace(%currLine, "\"marble/data", "$usermods @ \"/data");
+					%currLine = strreplace(%currLine, "\"platinum/data", "$usermods @ \"/data");
+					%currLine = strreplace(%currLine, "\"~/data", "$usermods @ \"/data");
+					
+					// this will auto correct any errors
+					%currLine = strreplace(%currLine, "\"$usermods @ \"/data", "$usermods @ \"/data");
+				}
+
+				%line[%count] = %currLine;
+				%count ++;
+			}
+		}
+		%fIn.close();
+
+		// now we write it out!
+		if (%fOut.openForWrite(%file))
+		{
+			for (%i = 0; %i < %count; %i ++)
+				%fOut.writeLine(%line[%i]);
+		}
+		%fOut.close();
+
+		%file = findNextFile(%search);
+	}
+
 	// cleanup
 	%fIn.delete();
 	%fOut.delete();
-	
-   echo("DONE!  Please restart marble blast.");
+
+	echo("DONE!  Please restart marble blast.");
 }
